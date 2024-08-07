f7cd1df2d37c9eea5ee1add594293abb938b9ef5
dinglierice <dinglie1997@163.com>
2024-08-07 23:19:27 +0800

    test

--- src/jvm/chtest/main.go
+++ src/jvm/chtest/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
+	fmt.Printf("接雨水")
 	height := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
 	fmt.Println(trap(height))
+
+	fmt.Println("合并区间")
+	intervals := [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}
+	fmt.Println(merge(intervals))
 }
 
 func trap(height []int) int {
@@ -25,4 +33,27 @@ func trap(height []int) int {
 		}
 	}
 	return totalVolume
+}
+
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	var ans [][]int
+	start := intervals[0][0]
+	end := intervals[0][1]
+
+	for i := 1; i < len(intervals); i++ {
+		newStart := intervals[i][0]
+		if newStart > end {
+			ans = append(ans, []int{start, end})
+			start = intervals[i][0]
+			end = intervals[i][1]
+		} else {
+			end = max(end, intervals[i][1])
+		}
+	}
+	ans = append(ans, []int{start, end})
+	return ans
 }
