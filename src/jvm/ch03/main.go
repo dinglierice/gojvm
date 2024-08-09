ef3898b53ddb397d6ce7e0b8683865332b170a49
dinglie <dinglie.dl@fliggy.com>
2024-08-09 19:38:41 +0800

    ch03适配

--- src/jvm/ch03/main.go
+++ src/jvm/ch03/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"zydl.edu/start/ch03/classfile"
 	"zydl.edu/start/ch03/classpath"
 )
 
@@ -33,6 +34,10 @@ func startJVM(cmd *Cmd) {
 	fmt.Printf("classpath:%s class:%s args:%v\n", cmd.cpOption, cmd.class, cmd.args)
 
 	className := strings.Replace(cmd.class, ".", "/", -1)
+	cf := loadClass(className, cp)
+	fmt.Printf("Load class:%s successfully\n", cmd.class)
+	printClassInfo(cf)
+
 	class, _, err := cp.ReadClass(className)
 	if err != nil {
 		fmt.Printf("Read class %s error:%v", className, err)
@@ -41,4 +46,34 @@ func startJVM(cmd *Cmd) {
 
 	// 正确加载了启动类
 	fmt.Printf("class data : %v\n", class)
+}
+
+func printClassInfo(cf *classfile.ClassFile) {
+	fmt.Println("Class Info:")
+	fmt.Printf("  Version: %d.%d\n", cf.MajorVersion(), cf.MinorVersion())
+	fmt.Printf("  constants count: %v\n", len(cf.ConstantPool()))
+	fmt.Printf("  Access flags: 0x%x\n", cf.AccessFlags())
+	fmt.Printf("  This class: %s\n", cf.ClassName())
+	fmt.Printf("  Super class: %s\n", cf.SuperClassName())
+	fmt.Println("  Interfaces:")
+	fmt.Println("  Fields:")
+	for _, f := range cf.Fields() {
+		fmt.Printf("    %s\n", f.Name())
+	}
+	fmt.Println("  Methods:")
+	for _, m := range cf.Methods() {
+		fmt.Printf("    %s\n", m.Name())
+	}
+}
+
+func loadClass(name string, cp *classpath.Classpath) *classfile.ClassFile {
+	classData, _, err := cp.ReadClass(name)
+	if err != nil {
+		panic(err)
+	}
+	cf, err := classfile.Parse(classData)
+	if err != nil {
+		panic(err)
+	}
+	return cf
 }
